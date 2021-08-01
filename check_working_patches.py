d70d4d8177ff0147a28ee9d81e6809dd6d55a956
Jack Bird <jack.bird@durham.ac.uk>
2021-08-01 20:58:25 +0100

    refresh repo made to accept multiple tools

--- check_working_patches.py
+++ check_working_patches.py
@@ -4,11 +4,15 @@ from shutil import copyfile
 
 SUCKLESS = "git://git.suckless.org/"
 SITES = "sites"
-DWM = "dwm"
+PATCH_PATHS = {
+    "dwm"   :   "dwm.suckless.org/patches",
+    "st"    :   "st.suckless.org/patches",
+    "dmenu" :   "tools.suckless.org/dmenu/patches"
+}
+TOOLS = PATCH_PATHS.keys()
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
-patchesDir = f'{dname}{SITES}/dwm.suckless.org/patches/'
 
 def git_reset_clean(path):
     cwd = os.getcwd()
@@ -22,54 +26,48 @@ def git_reset_clean(path):
     os.chdir(cwd)
 
 
-def updateRepos(path):
+def refreshRepo(path, tool, master='master'):
+    if tool not in TOOLS and tool != SITES:
+        raise ValueError(f'{tool} is not in the tool list: {TOOLS}')
+
     cwd = os.getcwd()
     os.chdir(path)
 
-    # Make sure the repos are downloaded
-    if not os.path.isdir(SITES):
-        print(f'Downloading {SUCKLESS}{SITES}')
-        process = subprocess.Popen(["git", "clone", f"{SUCKLESS}{SITES}"], stderr=subprocess.PIPE)
-        process.wait()
-        print('download complete')
-    if not os.path.isdir(DWM):
-        print(f'Downloading {SUCKLESS}{DWM}')
-        process = subprocess.Popen(["git", "clone", f"{SUCKLESS}{DWM}"], stderr=subprocess.PIPE)
+    # Make sure the repo is downloaded
+    if not os.path.isdir(tool):
+        url = f'{SUCKLESS}{tool}'
+        print(f'Downloading {url}')
+        process = subprocess.Popen(["git", "clone", f"{url}"], stderr=subprocess.PIPE)
         process.wait()
         print('download complete')
 
-    # update repos
-    os.chdir(DWM)
-    print(f'{DWM}:')
-    process = subprocess.Popen(["git", "pull", "origin", "master"], stderr=subprocess.PIPE)
-    process.wait()
-    os.chdir(f'../{SITES}')
-    print(f'{SITES}:')
-    process = subprocess.Popen(["git", "pull", "origin", "master"], stderr=subprocess.PIPE)
+    # Update repo
+    os.chdir(tool)
+    print(f'Updating {tool}:')
+    if not os.path.isdir('.git'):
+       print(f'ERROR: {os.path.join(path, tool)} not a git repo')
+       exit(1)
+    process = subprocess.Popen(["git", "pull", "origin", master], stderr=subprocess.PIPE)
     process.wait()
 
     os.chdir(cwd)
-    git_reset_clean(os.path.join(path, DWM))
+    git_reset_clean(os.path.join(path, tool))
 
 
-def diffWorks(patch_path, dwm_path):
+def diffWorks(patch_path, tool_path):
     cwd = os.getcwd()
     patch_path = os.path.abspath(patch_path)
 
-    os.chdir(dwm_path)
-    copyfile(patch_path, './dwm-patch.diff')
+    os.chdir(tool_path)
+    copyfile(patch_path, './patch.diff')
 
-    process = subprocess.Popen(["git", "apply", "dwm-patch.diff"], stderr=subprocess.PIPE)
+    process = subprocess.Popen(["git", "apply", "patch.diff"], stderr=subprocess.PIPE)
     process.wait()
     output = process.communicate()[1]
 
     os.chdir(cwd)
-    git_reset_clean(dwm_path)
+    git_reset_clean(tool_path)
 
     if output == b'':
         return True
     return False
-
-
-updateRepos('./')
-print(diffWorks('./sites/dwm.suckless.org/patches/notitle/dwm-notitle-6.2.diff', './dwm'))
