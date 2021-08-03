bd4b86f00384c2bd28b6162d0bb7895ceff7e362
Jack Bird <jack.bird@durham.ac.uk>
2021-08-03 10:26:44 +0100

    Started argument parser

--- check_working_patches.py
+++ check_working_patches.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import argparse
 from shutil import copyfile
 
 SUCKLESS = "git://git.suckless.org/"
@@ -13,6 +14,7 @@ TOOLS = PATCH_PATHS.keys()
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
+
 
 def git_reset_clean(path):
     cwd = os.getcwd()
@@ -71,3 +73,51 @@ def diffWorks(patch_path, tool_path):
     if output == b'':
         return True
     return False
+
+
+def listPatchDirs(tool, sites_path):
+    patch_path = os.path.join(sites_path, PATCH_PATHS[tool])
+    return os.listdir(patch_path)
+
+
+def listDiffs(path):
+    diffs = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            if file.endswith('.diff'):
+                diffs.append(os.path.join(root, file))
+    return diffs
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Checks if patches can be applied')
+    parser.add_argument('tool', help='suckless tool name to check patches on')
+    parser.add_argument('-p', '--patches', metavar='patch_name', default='all', dest='patches', help='list of patches to check seperated by a comma, defaults "all"')
+    parser.add_argument('-o', '--output', metavar='file', dest='output')
+    parser.add_argument('--tool', dest='tool_path', help='specify the location of the tool repo')
+    parser.add_argument('--sites', dest='sites_path', help='specify the location of the sites repo')
+
+    args = parser.parse_args()
+
+    #defaults for paths
+    if not args.tool_path:
+        args.tool_path = os.path.join(dname, args.tool)
+    if not args.sites_path:
+        args.sites_path = os.path.join(dname, SITES)
+
+
+    print(args.tool, args.tool_path)
+
+
+    #TODO
+    # Split refresh repo in to clone and update
+    # Clone first checks if the folder already exists, if not start the clone
+    # then update as normal
+    # this will allow for the tool and site path to be set by the user
+
+    refreshRepo(dname, args.tool)
+    refreshRepo(dname, SITES)
+
+
+if __name__ == '__main__':
+    main()
